c71ba2b12ec6582d31723995f3dff2cd9478305f
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2022-04-01 05:41:24 -0300

    implementação bem simples do u16
    O jeito de implementar tal é uma array-estática de dois bytes ser convertida no inteiro positivo de 16-bits. Funciona muito bem, porém precisa de mais testes para chegar-se num resultado razoável. Obviamente é bem mais lento, já que têm que fazer, no mínimo, uma iteração linear -- sendo sincero, não vi o código da transformação! -- para transformar tais no número.

--- src/aleatorio.rs
+++ src/aleatorio.rs
@@ -263,7 +263,7 @@ pub mod sortear {
    }
 
    // Será futuramente implementado.
-   fn _numero_u16() -> u16 { 
+   fn u16() -> u16 { 
       /* Mesmo acima, a distribuição é a
        * seguinte:
        * -- 55,536 números são de 5 algarismos.
@@ -280,7 +280,7 @@ pub mod sortear {
        *    está entre 0,14% dos números.
        * -- 10 contendo apenas 1 algarismos. Quase
        *    nada, só 0,015% dos números. */
-       0_u16
+       u16::from_to_be_bytes([u8(0..=255), u8(0..=255)])
    }
 }
 
