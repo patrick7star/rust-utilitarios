ba437ba8a775f2b2816a5d32d373ceca5822adf0
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2023-01-19 13:30:39 -0300

    documentação melhorada.

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,26 @@
  complexas, ou seus esboços iniciais e simples, 
  porém bem úteis ficarão aqui. Isto é muito melhor
  que ao invés de criar um `crate` para cada um.
+ Códigos que são também repetidos por várias `libs`,
+ serão colocadas aqui, por motivos óbvios.
   Como disse antes tais funções e estruturas de cada
  módulo executam coisas muitos simples.
+
+ Outra razão para termos isso aqui é, um jeito de 
+ burlar a internet e pacotes de terceiros. Toda 
+ vez que são compilados *caixotes* do **Rust**, aqueles
+ que depedem de pacotes teceiros, se não houver 
+ *artefatos* no computador, então procuram na 
+ internet, se não houver qualquer conexão, você 
+ não conseguirá compilar, consequentemente, executar
+ o programa; por isso é importante uma biblioteca 
+ que dependa menos e menos de internet, ou quase 
+ nada. Também ficam implementações do ***autor***,
+ que não são para ser um substituto, mas uma tentativa
+ de implementação própria, seja para aprendizado, ou
+ apenas diversão, e apromimoramento de novas técnicas
+ recentemente aprendidas da **linguagem Rust**, ou
+ de computação em geral.
 */
 
 
@@ -16,11 +34,11 @@
 */
 pub mod arvore;
 
+mod tela_auxiliar;
 /** talvez o mais complexo do pacote, cria uma estrura
  para manipular de forma maleável a impressão de
  texto e desenhos simples no terminal.
 */
-mod tela_auxiliar;
 pub mod tela;
 
 /** converte valores inteiros e fluantes, que representam
@@ -35,7 +53,10 @@ pub mod legivel;
 */
 pub mod tabelas;
 
-/// Simulação de valores aleatórios.
+/** Gerador de números pseudo-aleatórios. Todos
+ tipos de dados inteiros estão inclusos no pacote,
+ inclusive não-inteiros como valores de "decimais".
+ */
 pub mod aleatorio;
 
 /** Transforma strings e inteiros que representam valores
