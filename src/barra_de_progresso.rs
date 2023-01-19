62e021deda1ea0140060d60383aea18c97051dea
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2023-01-18 21:44:40 -0300

    refatoração.

--- src/barra_de_progresso.rs
+++ src/barra_de_progresso.rs
@@ -11,17 +11,27 @@ a ser atingida.
 use super::legivel::tamanho;
 use std::time::{Duration, Instant};
 use std::fmt::{Display, Formatter, Result as Result_fmt};
-use std::ops::{Add, AddAssign, Range};
+use std::ops::{Add, AddAssign};
 
 // própria biblioteca:
 use crate::legivel;
 use super::terminal_dimensao::{Largura, terminal_largura};
+
+// restante do módulo(re-exportando...):
+mod letreiro;
+mod unitarios;
+mod progressosimples;
+pub use letreiro::*;
+pub use progressosimples::*;
 
 // símbolo que representa frações das barras e espaços vázios.
 const COMPONENTE:&'static str ="#";
 const VAZIO:&'static str=".";
 const CAPACIDADE:u8 = 50;
 const TEXTO_MAX:usize = 30;
+
+/// Atalho para `ProgressoTemporal`.
+pub type PT = ProgressoTemporal;
 
 
 // cálcula número de algarismos de dado número.
@@ -44,6 +54,8 @@ fn conta_algs(valor:usize) -> u8 {
  dependendo de como foi escrito pode usar muita
  memória, e o consumo desnecesário de  `CPU` é 
  certo.*/
+#[deprecated(since="1.3.5", 
+note="usado agora apenas internamente")]
 pub fn progresso(qtd_atual:u64, qtd_total:u64) -> String {
    let percentagem:f32 = (qtd_atual as f32)/(qtd_total as f32);
    let percent_100:f32 = percentagem*100.0;
@@ -79,14 +91,15 @@ pub fn progresso(qtd_atual:u64, qtd_total:u64) -> String {
  numérica. Mesmo que o acima, o consumo de 
  `CPU` é dispedício, não use em **loops**.
  */
+#[deprecated(since="1.3.5", 
+note="usado agora apenas internamente")]
 pub fn progresso_data(qtd_atual:u64, qtd_total:u64) -> String {
    let percentagem:f32 = (qtd_atual as f32)/(qtd_total as f32);
    let percent_100:f32 = percentagem*100.0;
 
    // caso de erro.
-   if percentagem > 1.0_f32 {
-      panic!("os valores de atual supera o total!");
-   }
+   if percentagem > 1.0_f32 
+      { panic!("os valores de atual supera o total!"); }
    else if percentagem == 1.00 {
       let total_bytes = tamanho(qtd_total, true);
       return format!(
@@ -111,103 +124,6 @@ pub fn progresso_data(qtd_atual:u64, qtd_total:u64) -> String {
    }
 }
 
-/** É um letreiro dinâmico que dado um string
- a cada determinado tempo, as letras se movem
- da direita para esquerda, ou vice-versa. É 
- um objeto muito útil se todo o nome do progresso
- não cabe totalmente na tela.
-*/
-pub struct Logo<'a> {
-   // para marcar o tempo.
-   ti:Instant,
-   // o texto que será mostrado.
-   rotulo:&'a str,
-   // quando da string mostrar.
-   capacidade:u8,
-   // inicio e fim onde visualizar a string.
-   ponta_esquerda:u8,
-   ponta_direita:u8,
-   // intervalo válido.
-   intervalo:Option<Range<usize>>,
-}
-impl <'a> Logo<'a> {
-   // criando uma nova instância.
-   pub fn novo(label:&str) -> Result<Logo, &'static str> {
-      if label.len() == 0 {
-         Err("não é permitido strings em branco")
-      }
-      else {
-         Ok(
-            Logo {
-               // iniciando contagem.
-               ti: Instant::now(),
-               // pegando o rótulo a dimanizar.
-               rotulo: label,
-               // capacidade definida manualmente.
-               capacidade: 15, // quinze caractéres.
-               ponta_esquerda: 0,
-               ponta_direita: 15,
-               intervalo:Some(0..15),
-            }
-         )
-      }
-   }
-   // motor do logo. 
-   pub fn movimenta_letreiro(&mut self) {
-      // se chegou ao final, resetar posição do LED.
-      if self.ponta_direita == self.rotulo.len() as u8 {
-         self.ponta_direita = self.capacidade;
-         self.ponta_esquerda = 0;
-      }
-      // a cada 1,5seg mover o led 'uma casa'.
-      if self.ti.elapsed() > Duration::from_millis(500) {
-         if self.ponta_direita <= self.rotulo.len() as u8 {
-            // deslocando led...
-            self.ponta_esquerda += 1;
-            self.ponta_direita += 1;
-            // resetando contagem...
-            self.ti = Instant::now();
-         }
-      }
-      // definindo novo intervalo.
-      self.intervalo = {
-         // "renomeação" para melhor legibilidade.
-         let pe:usize = self.ponta_esquerda as usize;
-         let pd:usize = self.ponta_direita as usize;
-         Some(pe..pd)
-      };
-   }
-   // transforma numa slice-string.
-   pub fn para_string(&self) -> &'a str {
-      match self.intervalo.clone() {
-         Some(i) => {
-            self.rotulo
-            .get(i)
-            .unwrap()
-         },
-         None => self.rotulo,
-      }
-   }
-   // nova capacidade do logo.
-   pub fn nova_capacidade(&mut self, capacidade:u8) {
-      self.capacidade = capacidade;
-   }
-}
-impl Display for Logo<'_> {
-   fn fmt(&self, f:&mut Formatter<'_>) -> Result_fmt {
-      // apeliando para legibilidade.
-      match self.intervalo.clone() {
-         Some(i) => {
-            write!(
-               f, "{}...",
-               self.rotulo.get(i)
-               .unwrap()
-            )
-         },
-         None => write!(f, "{}", self.rotulo)
-      }
-   }
-}
 
 /** progresso com rótulo dinâmico, então quando
  o rótulo é muito maior do que cabe na tela, ele
@@ -215,6 +131,10 @@ impl Display for Logo<'_> {
  mercearias e shops. Ele também é de dados, o 
  resto do seu núcleo.
 */
+#[deprecated(
+   since="1.3.5", 
+   note="confuso, e raramente utilizado"
+)]
 pub fn progresso_data_rotulo<'a>(rotulo:&'a str, qtd_atual:u64, 
 qtd_total:u64) -> String {
    // cálculando a porcetagem.
@@ -544,11 +464,9 @@ tempo_atual:Duration, tempo_total:Duration) -> String {
    }
 }
 
-
 #[cfg(test)]
 mod tests {
    use super::*;
-   use std::thread::sleep;
 
    #[test]
    fn teste_progresso_com_rotulo() {
@@ -563,43 +481,5 @@ mod tests {
          logo.movimenta_letreiro();
       }
       assert!(true);
-   }
-
-   #[test]
-   fn letreiro_dinamico() {
-      // instanciando logo dinâmico...
-      let texto = "isso e apenas um texto de teste, entao nao entre em panico";
-      let mut logo = Logo::novo(texto).unwrap();
-      // marcador de tempo.
-      let t:Instant = Instant::now();
-      while t.elapsed() < Duration::from_secs(15) {
-         print!("\r{}", logo.para_string());
-         logo.movimenta_letreiro();
-      }
-      assert!(true);
-   }
-
-   #[test]
-   fn testando_funcao_que_gira() {
-      let texto = "eu adoro suco de caju";
-      let mut logo = Logo::novo(texto).unwrap();
-      // movimento o texto, dormindo de acordo com
-      // o tempo de translação dele(simulando tempo).
-      sleep(Duration::from_secs_f32(0.5));
-      logo.movimenta_letreiro();
-      sleep(Duration::from_secs_f32(0.5));
-      logo.movimenta_letreiro();
-      sleep(Duration::from_secs_f32(0.5));
-      logo.movimenta_letreiro();
-      // tirando trecho translado.
-      let parte_i = logo.para_string();
-      /* o previsto, levando o tempo, e, 
-       * lembrando que o "LED" tem 14 locais,
-       * tem que cair exatamente como a 
-       * frase abaixo.
-       */
-      let parte_ii = "adoro suco de c";
-      // verificando resposta.
-      assert_eq!(parte_i, parte_ii);
    }
 }
