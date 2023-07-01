c3771e11fd8cbbbc03913d6e634cc02687ee4f45
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2023-06-30 23:38:07 -0300

    incrementado com novas features
    função que cria string aleatória; e função que escolhe elemento de algum `collection`. Ainda precisam de retoques, e também precisam ser refatoradas.

--- src/aleatorio.rs
+++ src/aleatorio.rs
@@ -66,9 +66,157 @@ pub fn embaralha<B>(array: &mut Vec<B>) {
    }
 }
 
+pub fn escolhe<C, S>(colecao: S, tamanho: usize) -> Option<C>
+  where S: IntoIterator<Item=C> 
+{
+   let mut colecao_iter = colecao.into_iter();
+   if tamanho == 0
+      { return None; }
+   let escolha = randomico::usize(0..=(tamanho-1));
+   colecao_iter.nth(escolha)
+} 
+
+/* O mesmo que acima, mas com dois "porém's". O
+ * primeiro é que sua complexidade algoritímica 
+ * é O(n), já que despeja todo o conteúdo numa 
+ * nova array; a outra, que é uma vantagem, não 
+ * necessita do parametro 'tamanho' da coleção. */
+#[allow(non_snake_case)]
+pub fn escolheI<C, D>(colecao: C) -> Option<D>
+  where C: IntoIterator<Item=D>
+{
+   let mut array: Vec<D>;
+   array = colecao.into_iter().collect();
+   let tamanho = array.len();
+   if tamanho == 0
+      { return None; }
+   let ultimo = tamanho - 1;
+   /* escolhendo o índice nela, e removendo na mesma
+    * array o índice desejado. */
+   Some(array.swap_remove(randomico::usize(0..=ultimo)))
+}
+
+/* Quase o mesmo que o acima, porém exige a slice
+ * do objeto para fazer a seleção entre. Sem falar
+ * que, retorna a referência de tal, ao invés de 
+ * o elemento em sí. Entretanto, possívelmente só
+ * funciona com alguns 'collections'. */
+#[allow(non_snake_case)]
+pub fn escolheII<'s, D>(fatia: &'s [D]) -> Option<&'s D> {
+   let tamanho = fatia.len();
+   let ultimo = tamanho - 1;
+   fatia.get(randomico::usize(0..=ultimo))
+}
+
+type Intervalo = RangeInclusive<u8>;
+use std::ops::RangeInclusive;
+/** cria uma string ascii de forma aleatória. */
+pub fn string_ascii(code: Intervalo, comprimento: usize) 
+  -> Result<String, &'static str>
+{
+   let a = code.start();
+   let b = code.end();
+   if *a < 33 || *a >= 127 || *b >= 127
+      { return Err("não é um caractére válido!"); }
+   let mut string = String::with_capacity(comprimento);
+   for _ in 1..=comprimento {
+      let intervalo = code.clone();
+      let codigo = sortear::u8(intervalo);
+      match char::from_u32(codigo as u32) {
+         Some(caractere) =>
+            { string.push(caractere); }
+         None =>
+            { return Err("código inválido!"); }
+      };
+   }
+   Ok(string)
+}
+
+// que tipo de letra deseja-se.
+#[derive(Debug)]
+pub enum Modo {
+   Maiuscula,
+   Minuscula,
+   Ambos
+}
+
+#[derive(Debug)]
+pub enum Classe { 
+   Alfabeto(Modo), 
+   Numerico, 
+   Pontuacao,
+   // à implementar:
+   Acentuacao,
+   Pares,
+   Matematico
+}
+
+
+use std::collections::BTreeSet;
+use std::iter::FromIterator;
+// conjunto com inteiros positivos.
+type Codigos = BTreeSet<u8>;
+
+fn string_do_conjunto(codigos: Codigos, tamanho: usize) -> String {
+   let mut string = String::with_capacity(tamanho);
+   let total = codigos.len();
+   /* colocado tudo numa array para que se possa
+    * selecionar seus índices. */
+   let linear = codigos.iter().collect::<Vec<&u8>>();
+
+   // encher até alcançar o demandado.
+   while string.len() < tamanho {
+      let indice = randomico::usize(0..=(total-1));
+      let codigo: u32 = (*linear[indice]).into();
+      let caractere = char::from_u32(codigo).unwrap();
+      string.push(caractere);
+   }
+
+   // retornando a construção randômica.
+   string
+}
+
+pub fn string_classe(tipo: Classe, comprimento: usize)
+  -> Result<String, &'static str>
+{
+   match tipo {
+      Classe::Alfabeto(qual_tipo_de_string) => {
+         match qual_tipo_de_string {
+            Modo::Maiuscula => 
+               { string_ascii(0x41..=0x5A, comprimento) }
+            Modo::Minuscula => 
+               { string_ascii(0x61..=0x7A, comprimento) }
+            Modo::Ambos => {
+               let tipo_alfabetico: Classe;
+               // tipo de 'cap' no cara ou coroa.
+               if sortear::bool()
+                  { tipo_alfabetico = Classe::Alfabeto(Modo::Minuscula); }
+               else
+                  { tipo_alfabetico = Classe::Alfabeto(Modo::Maiuscula); }
+               /* chama função recursivamente, porém agora
+                * como os argumentos bem definidos. */
+               string_classe(tipo_alfabetico, comprimento)
+            }
+         } 
+      } 
+      Classe::Numerico =>
+         { string_ascii(0x30..=0x39, comprimento) }
+      Classe::Pontuacao => {
+         let codes = Codigos::from_iter(vec![
+            0x21, 0x22, 0x27, 0x2E,
+            0x3A, 0x3B, 0x3F, 0x5F
+         ].drain(..));
+         Ok(string_do_conjunto(codes, comprimento))
+      }
+      #[allow(unreachable_patterns)]
+      _ => unimplemented!()
+   }
+}
 
 #[cfg(test)]
 mod tests {
+   use super::{string_classe, Classe, Modo};
+
    #[test]
    #[should_panic]
    fn intervalo_passado_errado_u16() {
@@ -118,5 +266,90 @@ mod tests {
       };
 
       assert!(false);
+   }
+
+   #[test]
+   fn string_ascii_aleatoria_amostra() {
+      for _ in 1..=12 { 
+         let codigo_aleatorio = super::string_ascii(35..=113, 15);
+         println!("==> {}", codigo_aleatorio.unwrap() ); 
+      }
+   }
+
+   #[test]
+   fn string_classe_alfabeto() {
+      let tipo1 = Classe::Alfabeto(Modo::Maiuscula);
+      let tipo2 = Classe::Alfabeto(Modo::Minuscula);
+      let s = string_classe(tipo1, 45).unwrap();
+      let r =  string_classe(tipo2, 37).unwrap();
+      assert_eq!(s.len(), 45);
+      assert_eq!(r.len(), 37);
+      println!("==> {}\n==> {}", s, r); 
+   }
+
+   #[test]
+   fn string_especificadas_tres_primeiras() {
+      let entradas = vec![
+         string_classe(Classe::Alfabeto(Modo::Maiuscula), 45),
+         string_classe(Classe::Alfabeto(Modo::Minuscula), 41),
+         string_classe(Classe::Numerico, 26),
+         string_classe(Classe::Pontuacao, 26),
+      ];
+      for entry in entradas.iter()
+         {  println!("==> {}", entry.as_ref().unwrap() ); }
+      
+      assert!(
+         entradas[0].as_ref().unwrap().chars().all(
+         |c| c.is_ascii_alphabetic() 
+         && c.is_ascii_uppercase()
+      ));
+      assert!(
+         entradas[1].as_ref().unwrap().chars().all(
+         |c| c.is_ascii_alphabetic() 
+         && c.is_ascii_lowercase()
+      ));
+      assert!(
+         entradas[2].as_ref().unwrap().chars()
+         .all(|c| c.is_ascii_digit())
+      );
+      assert!(
+         entradas[3].as_ref().unwrap().chars()
+         .all(|c| c.is_ascii_punctuation())
+      );
+   }
+
+   #[test]
+   fn escolhe_com_vetor() {
+      let entrada = vec!['a', 'x', 'p'];
+      let t = entrada.len();
+      let saida = super::escolhe(entrada, t);
+      debug_assert!(
+         saida == Some('a') ||
+         saida == Some('x') ||
+         saida == Some('p'),
+         "saida = {:?}", saida
+      );
+   }
+
+   use std::collections::HashSet;
+   use std::iter::FromIterator;
+   #[test]
+   fn escolhe_com_hashset() {
+      let mut amostra = vec![
+        'A', 'j', 'M', 'b',
+        'K', 'l', 'V', 'o'
+      ];
+      let entrada = HashSet::<char>::from_iter(amostra.drain(..));
+      let t = entrada.len();
+      let funcao = super::escolhe;
+      assert!(
+         (1..=100)
+         .map(|_| funcao(entrada.clone(), t))
+         .any(|saida| 
+            saida == Some('l') ||
+            saida == Some('M') ||
+            saida == Some('b')
+         )
+      );
    }
 }
