129c9f5ca1445827eef2eda8864b15ea983d48f1
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2025-02-01 13:13:24 -0300

    importações quebrados por causa da renomeação

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,63 @@
 /*!
- # O que são estes Utilitários 
-   Todos códigos que não tiverem elaborações bem complexas, ou seus esboços 
- iniciais e simples, porém bem úteis ficarão aqui. Isto é muito melhor que 
- ao invés de criar um `crate` para cada um. Códigos que são também repetidos por várias `libs`, serão colocadas aqui, por motivos óbvios. Como disse 
- antes tais funções e estruturas de cada módulo executam coisas muitos 
+ # O que são estes Utilitários
+   Todos códigos que não tiverem elaborações bem complexas, ou seus esboços
+ iniciais e simples, porém bem úteis ficarão aqui. Isto é muito melhor que
+ ao invés de criar um `crate` para cada um. Códigos que são também repetidos por várias `libs`, serão colocadas aqui, por motivos óbvios. Como disse
+ antes tais funções e estruturas de cada módulo executam coisas muitos
  simples.
 
-   Outra razão para termos isso aqui é, um jeito de burlar a internet e 
- pacotes de terceiros. Toda vez que são compilados *caixotes* do **Rust**, 
- aqueles que depedem de pacotes teceiros, se não houver *artefatos* no 
- computador, então procuram na internet, se não houver qualquer conexão, 
- você não conseguirá compilar, consequentemente, executar o programa; por 
+   Outra razão para termos isso aqui é, um jeito de burlar a internet e
+ pacotes de terceiros. Toda vez que são compilados *caixotes* do **Rust**,
+ aqueles que depedem de pacotes teceiros, se não houver *artefatos* no
+ computador, então procuram na internet, se não houver qualquer conexão,
+ você não conseguirá compilar, consequentemente, executar o programa; por
  isso é importante uma biblioteca que dependa menos e menos de internet, ou
- quase nada. Também ficam implementações do ***autor***, que não são para 
- ser um substituto, mas uma tentativa de implementação própria, seja para 
- aprendizado, ou apenas diversão, e apromimoramento de novas técnicas 
+ quase nada. Também ficam implementações do ***autor***, que não são para
+ ser um substituto, mas uma tentativa de implementação própria, seja para
+ aprendizado, ou apenas diversão, e apromimoramento de novas técnicas
  recentemente aprendidas da **linguagem Rust**, ou de computação em geral.
 */
 
 
-/** Dado um diretório desenha uma árvore em string baseando nos arquivos e 
+/** Dado um diretório desenha uma árvore em string baseando nos arquivos e
  * diretórios do atual(raíz) e seus subdirs. */
 pub mod arvore;
 
-/** Talvez o mais complexo do pacote, cria uma estrura para manipular de 
+/** Talvez o mais complexo do pacote, cria uma estrura para manipular de
  * forma maleável a impressão de texto e desenhos simples no terminal. */
 pub mod tela;
 
-/** Converte valores inteiros e fluantes, que representam grandezas 
- * importantes em computação, para valores legíveis, tais na formatação de 
- * strings. 
+/** Converte valores inteiros e fluantes, que representam grandezas
+ * importantes em computação, para valores legíveis, tais na formatação de
+ * strings.
  */
 pub mod legivel;
 
-/** Pega lista e arrays de dados e faz uma tabela delas para simples 
+/** Pega lista e arrays de dados e faz uma tabela delas para simples
   * impressões.
   */
 pub mod tabelas;
 
-/** Gerador de números pseudo-aleatórios. Todos tipos de dados inteiros 
- * estão inclusos no pacote, inclusive não-inteiros como valores de 
- * "decimais". 
+/** Gerador de números pseudo-aleatórios. Todos tipos de dados inteiros
+ * estão inclusos no pacote, inclusive não-inteiros como valores de
+ * "decimais".
  */
 pub mod aleatorio;
 
-/** Transforma strings e inteiros que representam valores decimais/ou 
- * binários para números romanos, o inverso também, ou seja, romanos para 
+/** Transforma strings e inteiros que representam valores decimais/ou
+ * binários para números romanos, o inverso também, ou seja, romanos para
  * números decimais/inteiros. */
 pub mod romanos;
 
 /// Incrementa vários modos de impressão na tela.
 pub mod impressao;
 
-/** Variádos tipos de barras de progressos, que informam de forma dinâmica 
+/** Variádos tipos de barras de progressos, que informam de forma dinâmica
  * como a computabilidade de tais dados abordados está indo. */
 pub mod progresso;
 
-/** Obtém a dimensão do terminal que roda tal lib. Portanto agora, podemos 
- * excluir a biblioteca externa utilizada para fazer tal, substituindo por 
+/** Obtém a dimensão do terminal que roda tal lib. Portanto agora, podemos
+ * excluir a biblioteca externa utilizada para fazer tal, substituindo por
  * esta. */
 pub mod terminal;
 
@@ -68,7 +68,7 @@ pub mod porextenso;
 /** Macro importante para agilizar na criação
  de um Mapa(HashMap). A forma que tem que ser
  escrito é, dado dois tipos, lado esquerdo
- fica a chave, lado direito o valor. Até que 
+ fica a chave, lado direito o valor. Até que
  se saiba, funciona com quase todos tipos
  primitivos.
  ```
@@ -111,11 +111,11 @@ mod tests {
    #[test]
    fn converte_romano() {
       // número romano digitado.
-      let nr = super::lanca_prompt("digite um número romano");
+      let nr = super::terminal::lanca_prompt("digite um número romano");
       // conversão para decimal.
       let nd = super::romanos::romano_para_decimal(nr.as_str());
       // decimal esperado.
-      let de = super::lanca_prompt("qual o decimal esperado");
+      let de = terminal::lanca_prompt("qual o decimal esperado");
       let de = u16::from_str(de.as_str()).unwrap();
       println!("{} ==> {}", nr, nd);
       assert_eq!(de, nd);
@@ -136,7 +136,7 @@ mod tests {
          dicio.contains_key(&'b') &&
          dicio.contains_key(&'c') &&
          dicio.contains_key(&'z') &&
-         dicio.contains_key(&'y') 
+         dicio.contains_key(&'y')
       );
       // declaração embarca também outros tipos:
       let _dicio = mapa![
@@ -151,7 +151,7 @@ mod tests {
          _dicio.contains_key(&'b') &&
          _dicio.contains_key(&'c') &&
          _dicio.contains_key(&'z') &&
-         _dicio.contains_key(&'y') 
+         _dicio.contains_key(&'y')
       );
       let _dicio = mapa! {
          'a': 10,
@@ -165,7 +165,7 @@ mod tests {
          _dicio.contains_key(&'b') &&
          _dicio.contains_key(&'c') &&
          _dicio.contains_key(&'z') &&
-         _dicio.contains_key(&'y') 
+         _dicio.contains_key(&'y')
       );
    }
 }
