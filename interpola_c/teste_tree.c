4c0589b7d275a48e79f66fe69d2983e7c0fe5625
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2025-02-05 03:09:28 -0300

    incrementando, ..., porém ainda não pronto

--- interpola_c/teste_tree.c
+++ interpola_c/teste_tree.c
@@ -1,15 +1,18 @@
-#include "cextensoes.h"
+#include "interpola_c.h"
 #include <stdio.h>
 
-void cria_simples_arvore_e_imprime(char* path)
+void cria_simples_arvore_e_imprime(char* pathname)
 {
-   char* output = gera_arvore(path, true);
+   char* output = gera_arvore(pathname, true);
 
+   /* Imprime a árvore formada, então libera a string. Na formação, tal 
+    * string cuspida é construída dinamicamente, portanto, alocada na 
+    * heap do stack de memória. */
    printf("\n%s\n", output);
    free(output);
 }
 
-int main(int total, char* args[]) 
+int main(int total, char* args[], char* envs[]) 
 {
    if (total == 2) { 
       const char* caminho = args[1];
@@ -20,5 +23,5 @@ int main(int total, char* args[])
       abort();
    }
 
-   return 0;
+   return EXIT_SUCCESS;
 }
