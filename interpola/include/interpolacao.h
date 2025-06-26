3fd8943932effddb65b2d89a9311a2155c84106f
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-06-26 20:21:06 -0300

    Mais métodos da `Tabela`.

--- interpola/include/interpolacao.h
+++ interpola/include/interpolacao.h
@@ -3,11 +3,13 @@
 #define __C_EXTENSOES_H__
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
  /* Pega um número positivo inteiro, então escreve todo ele por extenso.
   * O retorno será uma array do tipo narrow caractére com sua representação
   * textual. */
  char* escrita_por_extenso(size_t numero); 
+
  /* Dado um caminho a um diretório arbitrário, e se colocado a opção de 
   * visualizar os arquivos. As funções abaixo formam toda ela(seus arquivos
   * e subdiretórios), no formato de árvore, com recuo dependendo da subpasta,
@@ -21,4 +23,34 @@
    const char** exclusao, int quantia
  );
 
+ /* Faz as tabelas de dadas colunas. Tais colunas são um tipo de estrutura
+  * que porta um 'rótulo', este dizendo o que tipo de dado ela retém, e 
+  * o 'rol' de determinado tamanho deste. */
+ typedef struct Coluna {
+   // Que tipo de dados estamos trabalhando.
+   const char* rotulo;
+   // Sequência de dados em sí, formatados como strings.
+   char** rol;  
+   // O comprimento deste 'rol'.
+   const int length;
+   // Maior string do 'rol'.
+   size_t largura;
+
+ } Coluna;
+
+ struct Coluna nova_col
+   (const char* rotulo, char** rol, int n);
+
+ size_t linhas_col  (struct Coluna);
+ size_t largura_col (struct Coluna);
+ void   free_col    (struct Coluna*);
+ void   debug_col   (struct Coluna);
+ void   print_col   (struct Coluna);
+
+ typedef struct Tabela { uint32_t *contador; } Tabela;
+
+ Tabela  nova_table     (bool);
+ void    adiciona_table (Tabela*, Coluna);
+ void    print_table    (Tabela* );
+ 
 #endif
